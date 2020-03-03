63f5a2a942d9faea55997ada4a24a7bd5486c7e1
Simon <simonsch.tz@gmail.com>
2020-03-03 15:45:41 +0100

    Split up content to Header.js & SearchResults.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,16 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Header from './Header';
+import SearchResults from './SearchResults';
 
 class App extends Component {
   render() {
     return (
-      <header>
-        <h1>UserSearch</h1>
-        <form>
-          <input type={"text"}></input>
-          <button type={"submit"}>Search</button>
-        </form>
-        <ul className="main-list">
-          <li>
-            <ul className="inner-list">
-              <li><h2>ID</h2></li>
-              <li>Dummy</li>
-              <li>Dummy</li>
-              <li>Dummy</li>
-            </ul>
-          </li>
-          <li>
-            <ul className="inner-list">
-              <li><h2>Name</h2></li>
-              <li>Dummy</li>
-              <li>Dummy</li>
-              <li>Dummy</li>
-            </ul>
-          </li>
-          <li>
-            <ul className="inner-list">
-              <li><h2>Email</h2></li>
-              <li>Dummy</li>
-              <li>Dummy</li>
-              <li>Dummy</li>
-            </ul>
-          </li>
+      <div>
+        <Header />
+        <SearchResults />
+      </div>
 
-
-        </ul>
-      </header>
     );
   }
 }
