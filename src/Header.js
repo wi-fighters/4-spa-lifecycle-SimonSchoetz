c0314b1579ad8474344b4b3ec187b6be9b135d03
Simon <simonsch.tz@gmail.com>
2020-03-03 15:46:01 +0100

    Split up content to Header.js & SearchResults.js

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header>
+                <h1>UserSearch</h1>
+            </header>
+        )
+    }
+}
---
+++ src/SearchResults.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+
+export default class SearchResults extends Component {
+    render() {
+        return (
+            <div className="results">
+                <form>
+                    <input type={"text"}></input>
+                    <button type={"submit"}>Search</button>
+                </form>
+                <ul className="main-list">
+                    <li>
+                        <ul className="inner-list">
+                            <li><h2>ID</h2></li>
+                            <li>Dummy</li>
+                            <li>Dummy</li>
+                            <li>Dummy</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <ul className="inner-list">
+                            <li><h2>Name</h2></li>
+                            <li>Dummy</li>
+                            <li>Dummy</li>
+                            <li>Dummy</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <ul className="inner-list">
+                            <li><h2>Email</h2></li>
+                            <li>Dummy</li>
+                            <li>Dummy</li>
+                            <li>Dummy</li>
+                        </ul>
+                    </li>
+
+
+                </ul>
+
+            </div>
+        )
+    }
+}
