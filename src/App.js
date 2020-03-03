1e3b6c6efd88578ea1a4f481709b4a5040994415
Simon <simonsch.tz@gmail.com>
2020-03-03 15:16:52 +0100

    Set up basic structure and styling

--- src/App.js
+++ src/App.js
@@ -4,9 +4,41 @@ import './App.scss';
 class App extends Component {
   render() {
     return (
-      <div>
-        Hallo World
-      </div>
+      <header>
+        <h1>UserSearch</h1>
+        <form>
+          <input type={"text"}></input>
+          <button type={"submit"}>Search</button>
+        </form>
+        <ul className="main-list">
+          <li>
+            <ul className="inner-list">
+              <li><h2>ID</h2></li>
+              <li>Dummy</li>
+              <li>Dummy</li>
+              <li>Dummy</li>
+            </ul>
+          </li>
+          <li>
+            <ul className="inner-list">
+              <li><h2>Name</h2></li>
+              <li>Dummy</li>
+              <li>Dummy</li>
+              <li>Dummy</li>
+            </ul>
+          </li>
+          <li>
+            <ul className="inner-list">
+              <li><h2>Email</h2></li>
+              <li>Dummy</li>
+              <li>Dummy</li>
+              <li>Dummy</li>
+            </ul>
+          </li>
+
+
+        </ul>
+      </header>
     );
   }
 }
